6c79bd09259c36f30f521e62920c8f96cadb0ab8
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-17 20:56:08 +0900

    開発環境セットアップ完了

---
+++ build/gcc-3.4.6/gcc/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --target=h8300-elf --disable-nls --disable-threads --disable-shared --disable-werror --enable-languages=c --prefix=/Users/kaito2/12step/tools";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
