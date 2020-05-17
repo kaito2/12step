339408a276023a4bf1307153ac796853983aa7c1
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-18 00:56:27 +0900

    code kata my01

---
+++ src/my01/bootload/main.c
@@ -0,0 +1,15 @@
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+int main(void)
+{
+    serial_init(SERIAL_DEFAULT_DEVICE);
+
+    puts("Hello kaito2!\n");
+
+    while (1)
+        ;
+
+    return 0;
+}
