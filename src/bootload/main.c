3c57ce597abd0abfafce29a0652e4d366fc1dfde
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-19 11:21:25 +0900

    Check static variable cannot be changed.

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -2,14 +2,21 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+
 int main(void)
 {
     serial_init(SERIAL_DEFAULT_DEVICE);
 
     puts("first line\n");
-    putxval(0x10, 0);
+
+    putxval(value, 0);
     puts("\n");
-    putxval(0xffff, 0);
+
+    // 静的変数の値の書き換えが反映されないことを確認
+    value = 20;
+
+    putxval(value, 0);
     puts("\n");
 
     while (1)
